1b9fd29d21d4dfef3041992168ec2ab5525b4daf
leicheng bai <xbfighting@hotmail.com>
2018-11-02 15:43:21 +0800

    sql mod

--- Sql/init.sql
+++ Sql/init.sql
@@ -8,3 +8,13 @@ CREATE TABLE `SocketData` (
   `createdAt` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=325 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `LotteryRecord` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `gameid` int(11) NOT NULL,
+  `result` int(11) NOT NULL,
+  `daxiao` varchar(10) NOT NULL DEFAULT '',
+  `danshuang` varchar(10) NOT NULL DEFAULT '',
+  `recordTime` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=649 DEFAULT CHARSET=utf8;
