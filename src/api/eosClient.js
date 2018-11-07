415a4e1902c19a1be919d77a27917dd57a9a8312
leicheng bai <xbfighting@hotmail.com>
2018-11-07 23:57:37 +0800

    eos api sample

---
+++ src/api/eosClient.js
@@ -0,0 +1,55 @@
+const logger = require('../logger');
+const { Api, JsonRpc, JsSignatureProvider } = require('eosjs');
+const fetch = require('node-fetch');
+const { TextDecoder, TextEncoder } = require('text-encoding');
+
+const privateKey = "5JAfem2e6us6xtcSiToZcSHB88HBdFMXDAYgwqR3gfrNTTXFTEW";
+const signatureProvider = new JsSignatureProvider([privateKey]);
+const rpc = new JsonRpc('https://api.eosnewyork.io', { fetch });
+const api = new Api({ rpc, signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder() });
+
+// 转账操作
+const transfer = async () => {
+  const result = await api.transact({
+    actions: [{
+      account: 'eosio.token',
+      name: 'transfer',
+      authorization: [{
+        actor: 'loveboyggggg', // TODO: para
+        permission: 'active',
+      }],
+      data: {
+        from: 'loveboyggggg', // TODO: para
+        to: 'eosplaybrand', // TODO: para
+        quantity: '0.1000 EOS', // TODO: para
+        memo: 'lottery:s', // TODO: para
+      },
+    }]
+  }, {
+    blocksBehind: 3,
+    expireSeconds: 30,
+  });
+  console.log(result);
+};
+
+
+const run = () => {
+  logger.info("process start...");
+
+  transfer().catch(err=>{
+    console.log("transfer error: ",err)
+  });
+
+  try {
+  } catch (err) {
+    logger.error(err);
+  }
+  logger.info("process done")
+  process.exit();
+}
+
+
+run()
+
+
+
